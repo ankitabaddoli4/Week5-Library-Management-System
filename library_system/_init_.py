f7bd746ec50cf27a293e633a0a02d2e42271f629
ankitabaddoli4 <ankitabaddoli4@gmail.com>
2026-03-14 16:20:19 +0530

    Add package docstring to _init_.py
    Added docstring to describe the library system package.

--- library_system/_init_.py
+++ library_system/_init_.py
@@ -1,1 +1,8 @@
+"""
+Library Management System Package
+This package contains modules for managing books, members, and library operations.
+"""
 
+from library_system.book import Book
+from library_system.member import Member
+from library_system.library import Library
